96423081af4771406abd26aa09e9c4958425addf
asama <asamahayder@yahoo.dk>
2019-11-25 09:19:52 +0100

    created packages for each layer in the application

--- app/src/main/java/com/example/noeglen/View/MainActivity.java
+++ app/src/main/java/com/example/noeglen/View/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.noeglen;
+package com.example.noeglen.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.noeglen.R;
 
 public class MainActivity extends AppCompatActivity {
 
