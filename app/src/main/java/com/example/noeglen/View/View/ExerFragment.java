478524de4854d54eb43581555f337eeb59d3f87d
ChrisMizz <Christofferdetlef@gmail.com>
2019-11-26 15:41:06 +0100

    Ændring af nogle xml filer

--- app/src/main/java/com/example/noeglen/View/View/ExerFragment.java
+++ app/src/main/java/com/example/noeglen/View/View/ExerFragment.java
@@ -15,6 +15,6 @@ public class ExerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        return inflater.inflate(R.layout.fragment_exer, container, false);
     }
 }
