dbae13b7da8ddd6286256ea80a31aee8a12851aa
Duyx <sandersen98@live.dk>
2020-01-10 14:51:09 +0100

    Added til struktur

--- app/src/main/java/com/example/noeglen/view/InfoArticleF.java
+++ app/src/main/java/com/example/noeglen/view/InfoArticleF.java
@@ -4,17 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.example.noeglen.R;
 
 public class InfoArticleF extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_infoarticle, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
