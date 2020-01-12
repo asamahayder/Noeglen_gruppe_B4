72d1fab39ba6d4105d98e7e9379d0960fbf98fc2
Thaer91 <thaerthaer1234@gmail.com>
2020-01-12 23:49:43 +0100

    Calendar

---
+++ app/src/main/java/com/example/noeglen/view/DiaryFCalendar.java
@@ -0,0 +1,54 @@
+package com.example.noeglen.view;
+
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.noeglen.R;
+
+public class DiaryFCalendar extends Fragment implements View.OnClickListener {
+
+    private static final String tag ="DiaryFCalendar";
+    private CalendarView calendarView;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        calendarView = getView().findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = i2 + "/" + (i1 + 1) + "/" +i ;
+                Log.d(tag, "onSelectedDayChange: day/month/year:"+ date);
+
+
+
+
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
