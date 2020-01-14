9c662c028dcdb82a6c5d164b97e4e389c4360bbb
JayVeeDee <jayveedees@gmail.com>
2020-01-14 15:05:15 +0100

    fikset navigation på dashmain

--- app/src/main/java/com/example/noeglen/view/MainActivity.java
+++ app/src/main/java/com/example/noeglen/view/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String fragmentTag, currDateString;
     private FragmentManager fm;
     private CurrentDate currDate;
+    private View currentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +82,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         Fragment selectedFragment = null;
+        currentView = v;
 
         for (int i = 0; i < 5; i++) {
             Button b = navBarBtnList.get(i);
@@ -151,12 +153,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         transaction.commit();
     }
 
+
     @Override
     public void inflateFragment(String tag) {
-        if (tag.equals(getString(R.string.fragment_dashvidmain))){
-            Fragment selectedFragment = new DashVidMainF();
-            setFragment(selectedFragment,tag,true, null);
-        }
+        checkNavBar(tag);
         if (tag.equals(getString(R.string.fragment_infoarticlesmain))){
             Fragment selectedFragment = new InfoArticlesMainF();
             setFragment(selectedFragment,tag,true, null);
@@ -177,17 +177,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Fragment selectedFragment = new DashVidF();
             setFragment(selectedFragment,tag,true, null);
         }
+    }
+
+    private void checkNavBar(String tag) {
+        View v = currentView;
+        if (tag.equals(getString(R.string.fragment_dashvidmain))){
+            Fragment selectedFragment = new DashVidMainF();
+            setFragment(selectedFragment,tag,true, null);
+        }
         if (tag.equals(getString(R.string.fragment_diarymain))){
             Fragment selectedFragment = new DiaryMainF();
             setFragment(selectedFragment,tag,true, null);
+            v = findViewById(R.id.bNav1);
         }
         if (tag.equals(getString(R.string.fragment_exermain))){
             Fragment selectedFragment = new ExerMainF();
             setFragment(selectedFragment,tag,true, null);
+            v = findViewById(R.id.bNav4);
         }
-
-
+        if (currentView != v){
+            onClick(v);
+        }
     }
+
 
     @Override
     public void onBackPressed() {
