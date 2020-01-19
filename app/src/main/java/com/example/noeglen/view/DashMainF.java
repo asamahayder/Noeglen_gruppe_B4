55192d998b33d9d68f8b4d03533aac77658415af
Asama <asamahayder@yahoo.dk>
2020-01-19 14:03:26 +0100

    Made changes to dashboard. getvideoList is now in dashoard main, however this will be moved over to log in in the next iteration.

--- app/src/main/java/com/example/noeglen/view/DashMainF.java
+++ app/src/main/java/com/example/noeglen/view/DashMainF.java
@@ -1,35 +1,18 @@
 package com.example.noeglen.view;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.view.GravityCompat;
-import androidx.core.view.ViewCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -39,14 +22,18 @@ import com.example.noeglen.R;
 import com.example.noeglen.data.ExerciseDTO;
 import com.example.noeglen.data.FavoritesDTO;
 import com.example.noeglen.data.KnowledgeDTO;
+import com.example.noeglen.data.MyCallBack;
+import com.example.noeglen.data.VideoDAO;
 import com.example.noeglen.data.VideoDTO;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Objects;
 
 public class DashMainF extends Fragment implements View.OnClickListener, DashMainRecyclerAdapter.OnFavoriteListener {
@@ -59,6 +46,13 @@ public class DashMainF extends Fragment implements View.OnClickListener, DashMai
     private FavoritesDTO favorites;
     private SharedPreferences sPref;
     private Gson gson;
+    private ImageView markTodaysVideoAsSeenImage;
+    private ImageView markTodaysDiaryAsWrittenImage;
+    private ImageView markTodaysExerciseAsDoneImage;
+    private boolean isNewDay;
+    private ArrayList<VideoDTO> videoList;
+    private ProgressBar progressBar;
+    private ConstraintLayout contentLayout;
 
     @Nullable
     @Override
@@ -71,15 +65,18 @@ public class DashMainF extends Fragment implements View.OnClickListener, DashMai
         super.onViewCreated(view, savedInstanceState);
         initializeView();
         iMain.visibilityShow();
-
     }
-
 
 
     private void initializeView() {
         iVidDash = Objects.requireNonNull(getView()).findViewById(R.id.iDashVid);
         iDiaryDash = Objects.requireNonNull(getView()).findViewById(R.id.iDashDiary);
         iExerciseDash = Objects.requireNonNull(getView()).findViewById(R.id.iDashExercise);
+        markTodaysVideoAsSeenImage = getView().findViewById(R.id.markTodaysVideoAsSeenImage);
+        markTodaysDiaryAsWrittenImage = getView().findViewById(R.id.markTodaysDiaryAsWrittenImage);
+        markTodaysExerciseAsDoneImage = getView().findViewById(R.id.markTodaysExerciseAsDoneImage);
+        progressBar = getView().findViewById(R.id.dashBoardProgressBar);
+        contentLayout = getView().findViewById(R.id.contentConstraintLayout);
         iVidDash.setOnClickListener(this);
         iDiaryDash.setOnClickListener(this);
         iExerciseDash.setOnClickListener(this);
@@ -91,6 +88,11 @@ public class DashMainF extends Fragment implements View.OnClickListener, DashMai
         adapter = new DashMainRecyclerAdapter(favorites,getContext(),this);
         rView.setLayoutManager(new LinearLayoutManager(getContext()));
         rView.setAdapter(adapter);
+
+        checkIfNewDay();
+        handleNewDay();
+        handleShowCheckMarks();
+
     }
 
     private void getSharedPref() {
@@ -156,6 +158,112 @@ public class DashMainF extends Fragment implements View.OnClickListener, DashMai
             iMain.setFragment(articleF,getString(R.string.fragment_infoknowledge),true,bundle);
             iMain.visibilityGone();
         }
+    }
+
+    public void checkIfNewDay(){
+        String preferenceKey = getString(R.string.sharedPreferencesKey);
+        String savedDateKey = getString(R.string.savedDateKey);
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+
+        SharedPreferences preferences = getActivity().getSharedPreferences(preferenceKey, Context.MODE_PRIVATE);
+        String savedDate = preferences.getString(savedDateKey,"2020-01-01");
+
+        if (!currentDate.equals(savedDate)){
+            isNewDay = true;
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(savedDateKey,currentDate);
+            editor.apply();
+        }else {
+            isNewDay = false;
+        }
+    }
+
+    public void handleNewDay(){
+        if (isNewDay){
+            String preferenceKey = getString(R.string.sharedPreferencesKey);
+            String isTodaysVideoSeenKey = getString(R.string.isTodaysVideoSeen);
+            String isTodaysDiaryWrittenKey = getString(R.string.isTodaysDiaryWritten);
+            String isTodaysExerciseDoneKey = getString(R.string.isTodaysExerciseDone);
+
+            SharedPreferences preferences = getActivity().getSharedPreferences(preferenceKey, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(isTodaysVideoSeenKey,"false");
+            editor.putString(isTodaysDiaryWrittenKey,"false");
+            editor.putString(isTodaysExerciseDoneKey,"false");
+            editor.apply();
+        }
+    }
+
+    public void handleShowCheckMarks(){
+        String isVideoSeen;
+        String isDiaryWritten;
+        String isExerciseDone;
+
+        String preferenceKey = getString(R.string.sharedPreferencesKey);
+        String isTodaysVideoSeenKey = getString(R.string.isTodaysVideoSeen);
+        String isTodaysDiaryWrittenKey = getString(R.string.isTodaysDiaryWritten);
+        String isTodaysExerciseDoneKey = getString(R.string.isTodaysExerciseDone);
+
+        SharedPreferences preferences = getActivity().getSharedPreferences(preferenceKey, Context.MODE_PRIVATE);
+
+        isVideoSeen = preferences.getString(isTodaysVideoSeenKey, "false");
+        isDiaryWritten = preferences.getString(isTodaysDiaryWrittenKey, "false");
+        isExerciseDone = preferences.getString(isTodaysExerciseDoneKey, "false");
+
+        if (isVideoSeen.equals("true")){
+            markTodaysVideoAsSeenImage.setVisibility(View.VISIBLE);
+        }
+        if (isDiaryWritten.equals("true")){
+            markTodaysDiaryAsWrittenImage.setVisibility(View.VISIBLE);
+        }
+        if (isExerciseDone.equals("true")){
+            markTodaysExerciseAsDoneImage.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public VideoDTO handleGetNextVideo(){
+        HashMap<String, Boolean> seenVideosList = getSeenVideosList();
+        for (int i = 0; i < videoList.size(); i++) {
+               if (!seenVideosList.get(videoList.get(i).getTitle())){
+                   return videoList.get(i);
+               }
+        }
+        return null;
+    }
+
+    public void setVideoList(ArrayList<VideoDTO> videoList){
+        this.videoList = videoList;
+    }
+
+    public void getAllVideosFromDataBase(){
+        VideoDAO videoDAO = new VideoDAO();
+        videoDAO.getAllVideos(new MyCallBack() {
+            @Override
+            public void onCallBack(Object object) {
+                setVideoList((ArrayList<VideoDTO>) object);
+                handleShowContent();
+            }
+        });
+    }
+
+    public HashMap<String, Boolean> getSeenVideosList(){
+        HashMap<String, Boolean> seenVideosList;
+        Gson gson = new Gson();
+        //Getting list from shared preferences
+        String preferenceKey = getString(R.string.sharedPreferencesKey);
+        String listKey = getString(R.string.seenVideosListKey);
+
+        SharedPreferences preferences = getActivity().getSharedPreferences(preferenceKey, Context.MODE_PRIVATE);
+        String listInJSON = preferences.getString(listKey, null);
+        Type type = new TypeToken<HashMap<String, Boolean>>(){}.getType(); //getting hashmap type for gson
+        seenVideosList = gson.fromJson(listInJSON, type);
+
+        return seenVideosList;
+    }
+
+    public void handleShowContent(){
+        progressBar.setVisibility(View.GONE);
+        contentLayout.setVisibility(View.VISIBLE);
     }
 
 }
